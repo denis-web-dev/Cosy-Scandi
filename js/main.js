a82e338148196c86647a39b7a4cbb75553751643
denis-web-dev <denis13.2011@mail.ru>
2025-02-13 12:41:53 +0500

    adaptive

--- js/main.js
+++ js/main.js
@@ -78,3 +78,14 @@ for (let btn of tabsBtns) {
     swiper.update()
   })
 }
+
+const mobileNavOpenBtn = document.querySelector('#open-mobile-nav-btn');
+const mobileNavCloseBtn = document.querySelector('#close-mobile-nav-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+
+mobileNavOpenBtn.onclick = function () {
+  mobileNav.classList.add('mobile-nav-wrapper--open');
+}
+mobileNavCloseBtn.onclick = function () {
+  mobileNav.classList.remove('mobile-nav-wrapper--open');
+};
